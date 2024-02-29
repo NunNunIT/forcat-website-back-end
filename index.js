914f4402c494599b1e8c4a6e9d9acc09ed023583
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-03-01 01:13:26 +0700

    Fix: fix cors

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser('secret'))
 
 // route init
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 route(app)
 
 app.listen(cfg.port, () => {
