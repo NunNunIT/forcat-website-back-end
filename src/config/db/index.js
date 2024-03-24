e3a456c84b63a2be167ccd1d3cb2f91af185b7a2
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-03-24 22:42:19 +0700

    update: account database user

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
-const url = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.gei9gq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${process.env.DATABASE_NAME}`;
+const url = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.gei9gq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const connect = async () => {
   try {
