d5f3cf3dc19ca3db7de100233bbce6f1e244e89f
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-04-06 05:34:36 +0700

    add: add cookie-parser and purchaseRoutes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import path from 'path';
+import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import productListRoutes from './routes/productList.route.js';
 import articleRoutes from './routes/article.route.js';
+import purchaseRoutes from './routes/purchase.route.js';
 
 const PORT = 8080;
 const __dirname = path.resolve()
@@ -26,6 +28,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
@@ -35,6 +38,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/productList', productListRoutes);
 app.use('/api/articles', articleRoutes);
+app.use('/api/purchases', purchaseRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
