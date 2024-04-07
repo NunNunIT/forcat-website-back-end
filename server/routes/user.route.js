52630495aedf9038b4d2eed52cd1984afdc16f72
Nubbynatt <21521415@gm.uit.edu.vn>
2024-04-07 15:57:07 +0700

    fix api user

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { edit, changePassword } from '../controllers/user.controller.js'
+import { edit, changePassword,  getUserByEmail, deleteUserByEmail } from '../controllers/user.controller.js'
 
 const router = express.Router();
 
+// 2 api này chỉ dùng để test edit và change resetPassword. không sử dụng trong thực tế
+router.get("/get-user", getUserByEmail);
+router.delete("/delete", deleteUserByEmail);
+
+//api được sử dụng
 router.put("/edit", edit);
 router.put("/change-password", changePassword);
 
