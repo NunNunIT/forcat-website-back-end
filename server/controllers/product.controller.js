ea69288431c499b139345b74f2aa1134aab7e5c2
namtuthien <leducmanh123kt@gmail.com>
2024-04-11 21:08:55 +0700

    update: populate Review in getProduct

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,4 +1,5 @@
-import Product from "../models/product.model.js"; // Import model sản phẩm
+import Product from "../models/product.model.js";
+import Review from "../models/review.model.js";
 import responseHandler from "../handlers/response.handler.js";
 import { decryptData } from "../utils/security.js";
 
@@ -7,7 +8,11 @@ export const getProduct = async (req, res, next) => {
   try {
     const encryptedProductId = req.params.pid;
     const productId = decryptData(encryptedProductId);
-    const product = await Product.findOne({ _id: productId });
+    const product = await Product.findOne({ _id: productId }).populate({
+      path: "recent_reviews",
+      select:
+        "_id product_variant_name review_rating user_info review_imgs review_video review_context createdAt",
+    });
 
     if (!product) {
       return responseHandler.notFound(res, "Product Not Found");
