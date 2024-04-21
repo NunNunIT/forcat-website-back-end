91f7a0360c7d8c6bb136344cfcc3ccc1af514002
NunNunIT <hongnhung02v04@gmail.com>
2024-04-22 03:11:07 +0700

    Fix Cors

--- server/index.js
+++ server/index.js
@@ -42,8 +42,9 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Header", 'Content-Type');
   next();
 });
 
