a6a7616c4cb46be392b6a2c64265249f0c11cbf6
NunNunIT <hongnhung02v04@gmail.com>
2024-04-23 05:31:20 +0700

    Fix JSON

--- server/middleware/verifyUser.js
+++ server/middleware/verifyUser.js
@@ -21,19 +21,19 @@ export const verifyAccessToken = (req, res, next) => {
 
 export const verifyUserAccessToken = async (req, res, next) => {
   const token = req.cookies.accessToken;
-  console.log("Real Token:", token)
+  console.log("FE send Token:", token);
   if (!token)
-    return responseHandler.unauthorize(res, 'You are not authenticated!');
+    return res.status(401).json({ error: 'You are not authenticated!' });
 
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const userData = await User.findById(user.id).select( '_id' );
+    const userData = await User.findById(user.id).select('_id');
     if (!userData) {
-      return responseHandler.notFound(res, 'User not exist!');
+      return res.status(404).json({ error: 'User not found!' });
     }
     // Nếu tất cả đều thành công, trả về dữ liệu với statusCode 200
-    return responseHandler.ok(res, "", "Authenticated user!");
+    return res.status(200).json({ message: 'Authenticated user!' });
   } catch (err) {
-    return responseHandler.error(res, err.message);
+    return res.status(500).json({ error: err.message });
   }
 }
