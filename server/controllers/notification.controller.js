57d0897d58fcf731f2a349643b020d32807a4ada
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-05-02 10:44:56 +0700

    debug(feature/notifications): console.log noti

--- server/controllers/notification.controller.js
+++ server/controllers/notification.controller.js
@@ -8,15 +8,25 @@ export const getAllNoti = async (req, res, next) => {
     return responseHandler.unauthorize(res, "You are not authenticated!");
   }
 
+  console.log(">> user_id:", user_id);
+
   const role = req.user?.role;
   if (!["admin", "staff", "user"].includes(role)) {
     return responseHandler.forbidden(res, "You are not authorized!");
   }
 
+  console.log(">> role:", role);
+
+
   const type = req.query?.type; // Lấy từ truy vấn loại thông báo từ query parameter
+
+  console.log(">> type:", type);
 
   const page = (parseInt(req.query?.page) > 0) ? parseInt(req.query?.page) : 1; // Trang mặc định là 1
   const limit = (parseInt(req.query?.limit) > 0) ? parseInt(req.query?.limit) : 10; // Số lượng thông báo trên mỗi trang
+
+  console.log(">> page:", page);
+  console.log(">> limit:", limit);
 
   const query = {
     $and: [
@@ -30,13 +40,21 @@ export const getAllNoti = async (req, res, next) => {
     ]
   };
 
+  console.log(">> query:", query);
+
   try {
     const maxPage = Math.ceil(await Notification.find(query).countDocuments() / limit); // Tính tổng số trang
+    if (maxPage === 0) {
+      return responseHandler.ok(res, { notifications: [], maxPage: 1 });
+    }
+
     const notifications = await Notification.find(query)
       .skip((page - 1) * limit) // Bỏ qua thông báo trên các trang trước đó;
       .limit(limit) // Giới hạn số lượng thông báo trên mỗi trang
       .sort({ createdAt: -1 })
       .exec();
+
+    console.log(">> notifications:", notifications);
 
     const handledNotifications = notifications.map(
       notification => {
@@ -58,6 +76,8 @@ export const getAllNoti = async (req, res, next) => {
         }
       }
     )
+
+    console.log(">> handled_notifications:", handledNotifications);
 
     return responseHandler.ok(res, { notifications: handledNotifications, maxPage });
   } catch (error) {
